182cc77543eb84f0ba8fcc269fcf73aeea84543c
chen098872-bin <chen098872@gmail.com>
2026-02-04 00:27:17 +0800

    Delete app directory

--- app/schemas/__init__.py
+++
@@ -1,8 +0,0 @@
-# schemas/__init__.py
-from pydantic import BaseModel, Field
-from typing import Annotated, Literal, Optional, Any
-
-class ResponseOut(BaseModel):
-    result: Annotated[Literal["success", "failure"], Field("success", description="操作结果")]
-    message: Optional[str] = Field(None, description="提示信息")
-    data: Optional[Any] = Field(None, description="返回数据")
